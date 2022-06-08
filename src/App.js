bf84b9f7891f238527f94d9d04723bed419e9a50
Marin <fantom2413@gmail.com>
2022-06-08 15:55:23 +0500

    Добавил инкремент и дикремент

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-    
-    <\/div>
-  );
+
+	const [state, setCount] = useState(5);
+	const [value, setValue] = useState("текст в инпуте");
+	function increment() {
+		setCount(state + 1);
+	}
+
+	function decrement() {
+		setCount(state - 1);
+	}
+
+	return (
+		<div className="App">
+			<h1>{state}</h1>
+
+			<button onClick={increment} >Increment</button>
+			<button onClick={decrement} >Decrement</button>
+
+		</div>
+	);
 }
 
 export default App;
