0bc016b651077b6c319fae6e0231591b57f82345
Marin <fantom2413@gmail.com>
2022-06-09 13:47:19 +0500

    новый компонент counter обновили app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { useState } from 'react';
-
+import Counter from './components/Counter';
 function App() {
-
-	const [state, setCount] = useState(5);
-	const [value, setValue] = useState("текст в инпуте");
-	function increment() {
-		setCount(state + 1);
-	}
-
-	function decrement() {
-		setCount(state - 1);
-	}
 
 	return (
 		<div className="App">
-			<h1>{state}</h1>
-
-			<button onClick={increment} >Increment</button>
-			<button onClick={decrement} >Decrement</button>
-
+			<Counter />
 		</div>
 	);
 }
