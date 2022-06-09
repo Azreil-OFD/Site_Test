9b8a38654254b1174df190939ffeeb6b30ffd1e8
Vadim Zarifullin <shira.vander@mail.ru>
2022-06-09 14:41:27 +0500

    Добавлена новая рабочая область (натаха). Новый jsx (PostItem). Package-lock (обновлен npm)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import Counter from './components/Counter';
+import PostItem from './components/PostItem';
 function App() {
 
 	return (
 		<div className="App">
-			<Counter />
+			<PostItem />
 		</div>
 	);
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const PostItem =  () => {
+
+
+    return (
+        <div>
+        <div className="post">
+            <div className="post__content">
+                <strong>1. Javascript</strong>
+                <div>
+                    Javascript - язык программирования
+                </div>
+            </div>
+            <div className="post__btns">
+                <button>УДАЛИТЬ</button>
+            </div>
+        </div>
+        </div>
+    );
+}
+
+export default PostItem;
